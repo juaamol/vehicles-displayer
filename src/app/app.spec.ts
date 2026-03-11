c8f10c526d1067c72ff2cad37c0856aed9874d58
jambulud <jambulud@gmail.com>
2026-03-11 23:20:55 +0100

    test(app): Check button and navbar

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,23 +1,37 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { App } from './app';
+import { By } from '@angular/platform-browser';
+import { ActivatedRoute, provideRouter } from '@angular/router';
 
 describe('App', () => {
+  let component: App;
+  let fixture: ComponentFixture<App>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
+      providers: [
+        provideRouter([]),
+        {
+          provide: ActivatedRoute,
+          useValue: {},
+        },
+      ],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(App);
+    component = fixture.componentInstance;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(App);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
   it('should render title', async () => {
-    const fixture = TestBed.createComponent(App);
-    await fixture.whenStable();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, vehicles-displayer');
+    const headers = fixture.debugElement.queryAll(By.css('span'));
+    const link = fixture.debugElement.query(By.css('a'));
+
+    expect(headers[0].nativeElement.textContent).toBe('Vehicles displayer');
+    expect(link.nativeElement.textContent).toBe('Brands catalog');
   });
 });
